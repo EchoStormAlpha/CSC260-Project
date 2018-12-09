1006fa1d0e32c09834b65a29d3e013416bf7f6b6
EchoStormAlpha <25696226+EchoStormAlpha@users.noreply.github.com>
2018-12-09 05:11:27 -0600

    Update Player_Collision.cs

--- OO Project/Assets/Player_Collision.cs
+++ OO Project/Assets/Player_Collision.cs
@@ -81,7 +81,11 @@ namespace Assets
                 {
                     player.currentItem = player.currentItem + 1;
                     Debug.Log(player.currentItem);
+<<<<<<< HEAD
                    gate1.currentItem= gate1.changeItem(player.currentItem);
+=======
+                
+>>>>>>> parent of fa35a1b... Minor tweaks and balances, increased encapsulation and abstraction, Added more notes
                 }
             }
     }
